41fb60ed33a35c5861e330c999bdd9655804293e
KawshithaFdo <praveenkawshika456@gmail.com>
2022-03-09 22:06:27 +0530

    Update Orrder Form

--- myprojects/Assignments/SPA/Pos/controller/OrderController.js
+++ myprojects/Assignments/SPA/Pos/controller/OrderController.js
@@ -5,3 +5,10 @@ function loadtabledata(item) {
 function clear() {
     $('#txtI_Id,#txtI_Name,#txtI_Price,#txtI_Qoh,#txtI_Qty').val("");
 }
+function updateqty(itemId,qty){
+    for (var i in dbItem){
+        if (dbItem[i].id==itemId){
+            dbItem[i].qty-=qty;
+        }
+    }
+}
